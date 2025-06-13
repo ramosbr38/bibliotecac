9d0105d1920bbc523a3ec35c6ed3e2dd81578cbb
Srnoob <bruno.ramos@universo.univates.br>
2025-06-13 20:10:48 -0300

    Add Categoria CRUD and API message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('JS carregado');
+
+async function carregarMensagem() {
+  try {
+    const resp = await fetch('https://api.adviceslip.com/advice');
+    const data = await resp.json();
+    document.getElementById('api-msg').textContent = data.slip.advice;
+  } catch (e) {
+    console.error('Erro ao consumir API', e);
+  }
+}
+
+if (document.getElementById('api-msg')) {
+  carregarMensagem();
+}
